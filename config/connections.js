d4d2bd80aa18b2440ee90bd735dc2b829a7b4541
duyetdev <me@duyetdev.com>
2015-08-09 16:56:18 +0700

    Config to using mongodb as default

--- config/connections.js
+++ config/connections.js
@@ -20,7 +20,7 @@
  */
 
 module.exports.connections = {
-
+  'default': 'mongo',
   /***************************************************************************
   *                                                                          *
   * Local disk storage for DEVELOPMENT ONLY                                  *
@@ -56,13 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  mongodbServer: {
-    adapter: 'feedback-flatform-dev',
+  mongo: {
+    adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
     // user: 'username',
     // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    database: 'feedback-flatform-dev'
   },
 
   /***************************************************************************
