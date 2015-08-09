576935fff07c62826fea8a9c0a2107d577734a89
duyetdev <me@duyetdev.com>
2015-08-09 16:00:50 +0700

    Add simple local Auth

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,18 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+
+  // Authentication route
+  'get /login': 'AuthController.login',
+  'get /logout': 'AuthController.logout',
+  'get /register': 'AuthController.register',
+
+  'post /auth/local': 'AuthController.callback',
+  'post /auth/local/:action': 'AuthController.callback',
+
+  'get /auth/:provider': 'AuthController.provider',
+  'get /auth/:provider/callback': 'AuthController.callback',
 
   /***************************************************************************
   *                                                                          *
