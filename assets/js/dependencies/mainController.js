6bb91542be39c4511302c38f595f17482edeaa18
duyetdev <me@duyetdev.com>
2015-08-09 18:28:10 +0700

    Add Angular Controller

---
+++ assets/js/dependencies/mainController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('feedbackPlatform', [
+
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
