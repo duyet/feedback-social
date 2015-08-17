b959ce20e66538e0c2ded5910120cbf52ff21f1e
Van-Duyet Le <lvduit08@gmail.com>
2015-08-17 16:44:06 +0700

    Login form check some information before send back to server

--- assets/js/v1/backbone/view/LoginForm.js
+++ assets/js/v1/backbone/view/LoginForm.js
@@ -12,13 +12,44 @@ define(function(require) {
         },
 
         doLogin: function(e) {
+            // Hidden previous message 
+            $("#messageBox").css('display', 'none');
+            
         	// TODO: Validate
         	var email = $('input[name="email"]').val();
         	var password = $('input[name="password"]').val();
-
-        	console.log("Login: ", email, password);
+            var _csrf = __c._csrf || '';
+            
+            // Checking 
+            if (!email || !password || email.length < 4 || password.length < 4) {
+                this.showMessage("warning", "Vui lòng điền chính xác username và password!");
+                return false;
+            }
+            if (!this.isEmail(email)) {
+                this.showMessage("danger", "Email không chính xác!");
+                return false; 
+            }
+            
+            this.showMessage("success", "Login with " + email + "...");
 
         	return false;
+        },
+        
+        showMessage: function(messageType, messageContent, next) {
+              var messageBox = $("#messageBox");
+              
+              messageBox.removeClass();
+              messageBox.addClass("alert alert-" + messageType);
+              messageBox.html(messageContent);
+              messageBox.css('display', 'block');
+              
+              if (next) next();
+              return true;
+        },
+        
+        isEmail: function(email) {
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test(email);
         },
     });
 });
