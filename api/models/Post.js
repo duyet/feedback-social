fa4aade153c490dda888ef53d6d4905627858749
Van-Duyet Le <lvduit08@gmail.com>
2015-08-18 19:30:43 +0700

    Add Post api

---
+++ api/models/Post.js
@@ -0,0 +1,24 @@
+/**
+* Post.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    title : { type: 'string', unique: true },
+    alias: { type: 'string', unique: true },
+    content: { type: 'string' },
+    created : { type: 'datetime' },
+    lastEdit: { type: 'datetime' },
+    state: {
+		type: 'string',
+		enum: ['publish', 'deleted', 'draft'],
+		default: 'publish'
+	}
+    author : { collection: 'User' }
+  }
+};
+
