4310ae9cea6da15d56e95e74ed1ac1658b7e30f8
duyetdev <me@duyetdev.com>
2015-08-19 02:02:03 +0700

    Add EditpostView as draft version

---
+++ assets/js/v1/admin/view/Manager/EditPost.js
@@ -0,0 +1,13 @@
+define(function(require) {
+    var Backbone = require('backbone');
+    var Post = require('model/PostModel');
+
+    return Backbone.View.extend({
+        initialize: function(options) {
+			
+		},
+
+        render: function(posts) {
+        }
+    });
+});
