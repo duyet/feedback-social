b8b7032da653a1bb2950fe1850cb6f11bc3cad44
Van-Duyet Le <lvduit08@gmail.com>
2015-08-19 16:06:06 +0700

    Add feedback comment api

---
+++ api/controllers/FeedbackCommentController.js
@@ -0,0 +1,11 @@
+/**
+ * FeedbackCommentController
+ *
+ * @description :: Server-side logic for managing Feedbackcomments
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
