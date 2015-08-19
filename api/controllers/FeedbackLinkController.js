7e950f26a521a45839e2fdd3d6831f68fb64351c
Van-Duyet Le <lvduit08@gmail.com>
2015-08-19 16:06:39 +0700

    Add feedback link api

---
+++ api/controllers/FeedbackLinkController.js
@@ -0,0 +1,11 @@
+/**
+ * FeedbackLinkController
+ *
+ * @description :: Server-side logic for managing Feedbacklinks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
