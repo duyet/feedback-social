4b93c2afdc4499219f99006b06529f854fde03e2
Van-Duyet Le <lvduit08@gmail.com>
2015-08-19 12:57:19 +0700

    Show 404 not found page when post not found

--- assets/js/v1/client/view/ViewPost.js
+++ assets/js/v1/client/view/ViewPost.js
@@ -1,12 +1,21 @@
 define(function(require) {
     var Backbone = require('backbone');
     return Backbone.View.extend({
+        initialize: function() {
+            if (this.model) {
+                this.model.on('change', this.render, this);
+            }
+        },
+        
         render: function() {
+            if (!this.model.id) {
+                this.$el.html(JST["assets/templates/notfound.html"]());
+                return this;
+            }
+            
         	this.$el.html(JST["assets/templates/view-post.html"]({
             	model: this.model
             }));
-
-        	$('#post-title').text(this.model.get('title'));
 
             return this;
         }
