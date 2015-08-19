746a9daa5e6e7a9eaa505db8af26eb07cd0055e7
Van-Duyet Le <lvduit08@gmail.com>
2015-08-19 16:06:51 +0700

    Add feedback controller

---
+++ api/controllers/FeedbackController.js
@@ -0,0 +1,11 @@
+/**
+ * FeedbackController
+ *
+ * @description :: Server-side logic for managing feedbacks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
