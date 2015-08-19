abb2243f9a4f0f26efb550f5955e6ae62da66aa8
Van-Duyet Le <lvduit08@gmail.com>
2015-08-19 16:07:50 +0700

    Fix error by remove tooltip, add this later

--- assets/js/v1/admin/view/Manager.js
+++ assets/js/v1/admin/view/Manager.js
@@ -1,7 +1,6 @@
 define(function(require) {
     var Backbone = require('backbone');
     var $ = require('jquery');
-    var Tooltip  = require('bootstrap/tooltip');
     return Backbone.View.extend({
         render: function() {
             this.$el.html(JST["assets/admin_templates/manager-main.html"]());
