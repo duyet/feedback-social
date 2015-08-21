0417beccdd34de18e0ff2d91242add179b3d3327
Van-Duyet Le <lvduit08@gmail.com>
2015-08-21 18:21:48 +0700

    Feedback api, find from alias

--- api/controllers/FeedbacksController.js
+++ api/controllers/FeedbacksController.js
@@ -6,6 +6,14 @@
  */
 
 module.exports = {
-	
+	findFromAlias: function(req, res) {
+		var _alias = req.params.id || '';
+		console.log(_alias);
+		Feedbacks.findOne({alias: _alias}, function(err, model) {
+			if (err || !model) return res.json(404, {});
+			
+			return res.json(model);
+		});
+	},
 };
 
