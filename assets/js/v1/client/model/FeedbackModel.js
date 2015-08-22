05275df8247783be38b2817c8ff8d2a549565588
duyetdev <me@duyetdev.com>
2015-08-22 17:22:25 +0700

    Update Backbone FeedbackModel

--- assets/js/v1/client/model/FeedbackModel.js
+++ assets/js/v1/client/model/FeedbackModel.js
@@ -3,16 +3,16 @@ define(function(require) {
 	var __c = window.__c || {};
 	
     return Backbone.Model.extend({
-		url: __c.api_prefix + '/feedbacks',
+		url: __c.api_prefix + '/feedbacks/create',
 		
 		listFeedback : function() {
-			this.url += '/lite'
+			this.url =  __c.api_prefix + '/feedbacks/lite'
 			
 			return this.fetch();
 		},
 
 		getByAlias: function(alias){
-			this.url = this.url + "/" + alias;
+			this.url = __c.api_prefix + '/feedbacks' + '/' + alias;
 			return this.fetch();
 		}
     });
