1e769817f50c73aaaff2e1ae946e0430eb5593d4
duyetdev <me@duyetdev.com>
2015-08-23 00:18:53 +0700

    Feedback vote and counter api

---
+++ api/models/FeedbackVote.js
@@ -0,0 +1,16 @@
+/**
+* FeedbackVote.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	feedback_post: { model: 'Feedbacks' },
+  	user: { model: 'user', via: 'feedback_comment_by' },
+  	vote_type: { type: 'string', enum: ['up', 'down'], defaultsTo: 'up' }, // 'up' or 'down'
+  }
+
+};
