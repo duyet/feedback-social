69143c05898f13d1f95036dbacc177d9e5b94ba4
duyetdev <me@duyetdev.com>
2015-08-25 02:07:12 +0700

    Fix policies for feedback comment load

--- config/policies.js
+++ config/policies.js
@@ -60,7 +60,8 @@ module.exports.policies = {
     },
 
     FeedbackCommentController: {
-        '*': 'hasToken',  
+        '*': 'hasToken',
+        'byFeedbackAlias': true
     },
 
     UserController: {
