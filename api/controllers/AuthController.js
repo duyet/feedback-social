3a8544a94fa104aa9ad3f507ebd59adb1cc33642
Van-Duyet Le <lvduit08@gmail.com>
2015-08-25 17:34:35 +0700

    Facebook login, draft version

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -13,51 +13,82 @@ var passport = require('passport');
  * @private
  */
 function _onPassportAuth(req, res, error, user, info) {
-    if (error) return res.serverError(error);
-    if (!user) return res.unauthorized(null, info && info.code, info && info.message);
+	console.log("=========> ", error, user, info);
+	if (error) return res.serverError(error);
+	if (!user) return res.unauthorized(null, info && info.code, info && info.message);
 
-    return res.ok({
-        // TODO: replace with new type of cipher service
-        token: CipherService.createToken(user),
-        user: user
-    });
+	return res.ok({
+		// TODO: replace with new type of cipher service
+		token: CipherService.createToken(user),
+		user: user
+	});
 }
 
 module.exports = {
-    /**
-     * Sign up in system
-     * @param {Object} req Request object
-     * @param {Object} res Response object
-     */
-    signup: function(req, res) {
-        User
-            .create(_.omit(req.allParams(), 'id'))
-            .then(function(user) {
-                return {
-                    // TODO: replace with new type of cipher service
-                    token: CipherService.createToken(user),
-                    user: user
-                };
-            })
-            .then(res.created)
-            .catch(res.serverError);
-    },
+	/**
+	 * Sign up in system
+	 * @param {Object} req Request object
+	 * @param {Object} res Response object
+	 */
+	signup: function(req, res) {
+		User
+			.create(_.omit(req.allParams(), 'id'))
+			.then(function(user) {
+				return {
+					// TODO: replace with new type of cipher service
+					token: CipherService.createToken(user),
+					user: user
+				};
+			})
+			.then(res.created)
+			.catch(res.serverError);
+	},
 
-    /**
-     * Sign in by local strategy in passport
-     * @param {Object} req Request object
-     * @param {Object} res Response object
-     */
-    signin: function(req, res) {
-        passport.authenticate('local',
-            _onPassportAuth.bind(this, req, res))(req, res);
-    },
-    
-    signout: function(req, res) {
-        req.logout();  
-        
-        res.ok({
-            message: 'logout'
-        });
-    },
+	/**
+	 * Sign in by local strategy in passport
+	 * @param {Object} req Request object
+	 * @param {Object} res Response object
+	 */
+	signin: function(req, res) {
+		passport.authenticate('local', _onPassportAuth.bind(this, req, res))(req, res);
+	},
+
+	/**
+	 * Sign out to all
+	 * @param {Object} req Request object
+	 * @param {Object} res Response object
+	 */
+	signout: function(req, res) {
+		req.logout();
+
+		res.ok({
+			message: 'logout'
+		});
+	},
+
+	facebook: function(req, res, next) {
+		console.log("------------ FACEBOOK --------------");
+		passport.authenticate('facebook', {
+			scope: ['email', /*'user_about_me'*/]
+		},
+		function(err, user) {
+			req.logIn(user, function(err) {
+				if (err) {
+					req.session.flash = 'There was an error';
+					console.log("Error");
+					res.redirect('user/login');
+				} else {
+					req.session.user = user;
+					console.log(req, res);		
+					//res.redirect('/user/dashboard');
+				}
+			});
+		})(req, res, next);
+
+	},
+
+	facebook_callback: function(req, res, next) {
+		console.log("------------ CALLBACK --------------");
+		passport.authenticate('facebook', _onPassportAuth.bind(this, req, res))(req, res, next);
+	},
 };
--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@
  */
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var JwtStrategy = require('passport-jwt').Strategy;
 
 var EXPIRES_IN_MINUTES = 60 * 24;
@@ -11,67 +12,162 @@ var ALGORITHM = "HS256";
 var ISSUER = "phanhoi.xyz";
 var AUDIENCE = "phanhoi.xyz";
 
+var FACEBOOK_CLIENT_KEY = "344116659122210";
+var FACEBOOK_SECRET_KEY = "211d877bd33806f9d42fa8988c2c779f";
+var FACEBOOK_CALLBACK_URL = "http://phanhoi.xyz:1337/api/v1/auth/facebook_callback";
+
 /**
  * Configuration object for local strategy
  */
 var LOCAL_STRATEGY_CONFIG = {
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: false
+	usernameField: 'email',
+	passwordField: 'password',
+	passReqToCallback: false
 };
 
 /**
  * Configuration object for JWT strategy
  */
 var JWT_STRATEGY_CONFIG = {
-    secretOrKey: SECRET,
-    issuer: ISSUER,
-    audience: AUDIENCE,
-    passReqToCallback: false
+	secretOrKey: SECRET,
+	issuer: ISSUER,
+	audience: AUDIENCE,
+	passReqToCallback: false
 };
 
 /**
  * Triggers when user authenticates via local strategy
  */
 function _onLocalStrategyAuth(email, password, next) {
-    var criteria = (email.indexOf('@') === -1) ? {username: email} : {email: email};
-    User.findOne(criteria)
-        .exec(function(error, user) {
-            if (error) return next(error, false, {});
+	var criteria = (email.indexOf('@') === -1) ? {
+		username: email
+	} : {
+		email: email
+	};
+	User.findOne(criteria)
+		.exec(function(error, user) {
+			if (error) return next(error, false, {});
 
-            if (!user) return next(null, false, {
-                code: 'E_USER_NOT_FOUND',
-                message: email + ' is not found'
-            });
+			if (!user) return next(null, false, {
+				code: 'E_USER_NOT_FOUND',
+				message: email + ' is not found'
+			});
 
-            // TODO: replace with new cipher service type
-            if (!CipherService.comparePassword(password, user))
-                return next(null, false, {
-                    code: 'E_WRONG_PASSWORD',
-                    message: 'Password is wrong'
-                });
+			// TODO: replace with new cipher service type
+			if (!CipherService.comparePassword(password, user))
+				return next(null, false, {
+					code: 'E_WRONG_PASSWORD',
+					message: 'Password is wrong'
+				});
 
-            return next(null, user, {});
-        });
+			return next(null, user, {});
+		});
 }
 
 /**
  * Triggers when user authenticates via JWT strategy
  */
 function _onJwtStrategyAuth(payload, next) {
-    var user = payload.user;
-    return next(null, user, {});
+	var user = payload.user;
+	return next(null, user, {});
+}
+
+
+/**
+ * Find user by user id
+ */
+function findById(id, next) {
+	User.findOne(id).exec(function(err, user) {
+		if (err)
+			return next(null, null);
+		else
+			return next(null, user);
+	});
+}
+
+/**
+ * Find user by facebook id
+ */
+function findByFacebookId(id, next) {
+	User.findOne({
+		socialProfiles: {
+			facebook: {
+				profile_id: id
+			}
+		}
+	}).exec(function(err, user) {
+		if (err) {
+			return next(null, null);
+		} else {
+			return next(null, user);
+		}
+	});
 }
 
 passport.use(
-    new LocalStrategy(LOCAL_STRATEGY_CONFIG, _onLocalStrategyAuth));
+	new LocalStrategy(LOCAL_STRATEGY_CONFIG, _onLocalStrategyAuth));
 passport.use(
-    new JwtStrategy(JWT_STRATEGY_CONFIG, _onJwtStrategyAuth));
+	new JwtStrategy(JWT_STRATEGY_CONFIG, _onJwtStrategyAuth));
+
+passport.use(new FacebookStrategy({
+	clientID: FACEBOOK_CLIENT_KEY,
+	clientSecret: FACEBOOK_SECRET_KEY,
+	callbackURL: FACEBOOK_CALLBACK_URL,
+	enableProof: true
+}, function(accessToken, refreshToken, profile, done) {
+	console.log(accessToken, refreshToken, profile, done);
+	findByFacebookId(profile.id, function(err, user) {
+
+		// Create a new User if it doesn't exist yet
+		if (!user) {
+			var newProfile = {
+				username: profile.username || '',
+				email: profile.email || '',
+				displayName: profile.displayName || '',
+				socialProfiles: {
+					facebook: {
+						profile_id: profile.id,
+						accessToken: accessToken || '',
+						refreshToken: refreshToken || '',
+					}
+				}
+			};
+			console.log("========================<> ", profile);
+			console.log("========================<> ", newProfile);
+
+			User.create(
+				newProfile
+			).exec(function(err, user) {
+				if (user) {
+					console.log(null, user, {
+						message: 'Logged In Successfully'
+					});
+					return done(null, user, {
+						message: 'Logged In Successfully'
+					});
+				} else {
+					console.log(err, null, {
+						message: 'There was an error logging you in with Facebook'
+					});
+					return done(err, null, {
+						message: 'There was an error logging you in with Facebook'
+					});
+				}
+			});
+
+			// If there is already a user, return it
+		} else {
+			return done(null, user, {
+				message: 'Logged In Successfully'
+			});
+		}
+	});
+}));
 
 module.exports.jwtSettings = {
-    expiresInMinutes: EXPIRES_IN_MINUTES,
-    secret: SECRET,
-    algorithm: ALGORITHM,
-    issuer: ISSUER,
-    audience: AUDIENCE
+	expiresInMinutes: EXPIRES_IN_MINUTES,
+	secret: SECRET,
+	algorithm: ALGORITHM,
+	issuer: ISSUER,
+	audience: AUDIENCE
 };
